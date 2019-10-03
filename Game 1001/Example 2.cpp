f7659684018be2fbd2de909a310c1e6a68d63b8c
Aidan Lee-Dadswell <101249681@georgebrown.ca>
2019-10-03 13:16:00 -0400

    Added example 2

---
+++ Game 1001/Example 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 4;
+
+	cout << num   << "    ";
+	cout << num++ << "    ";
+	cout << num   << "    ";
+	cout << ++num << "    ";
+
+	system("Pause");
+	return 0;
+}
